7d84646092c81bd93ff776be98f904a62ded2805
UgrasK <ugurkaraer1989@gmail.com>
2023-08-22 01:53:39 +0300

    Add project files.

---
+++ ExtensionAndRecursiveMethodsDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionAndRecursiveMethodsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Recursive recursive = new Recursive();
+            int result = recursive.PowerOfNum(11, 3);
+            Console.WriteLine(result);
+
+            string text1 = "Julius Caesar";
+            Console.WriteLine(text1.CheckSpace());
+
+            Console.ReadLine();
+
+        }
+    }
+
+    public class Recursive
+    {
+        public int PowerOfNum(int num, int power)
+        {
+            if (power == 1)
+            {
+                return num;
+            }
+            return PowerOfNum(num, power - 1) * num;
+        }
+    }
+
+    public static class Extension
+    {
+        public static bool CheckSpace(this string text)
+        {
+            return text.Contains(" ");
+        }
+    }
+}
